18307d5d98b31d10ba8e7983d74411edc15aa25d
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-04-01 14:33:56 -0700

    Add category enum and operation enum

---
+++ PatientManagement/Enum.swift
@@ -0,0 +1,16 @@
+//
+//  Enum.swift
+//  PatientManagement
+//
+//  Created by xiyuan tu on 4/1/24.
+//
+
+import Foundation
+
+enum Category: String, Equatable {
+    case PATIENT, EXAM
+}
+
+enum Operation: String, Equatable {
+    case ADD, DEL
+}
