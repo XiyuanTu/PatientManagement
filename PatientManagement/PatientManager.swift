c2c959b42b5973e0e984a359159434984ad77fee
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-04-01 14:43:01 -0700

    Implement process, processPatientData, processExamData

--- PatientManagement/PatientManager.swift
+++ PatientManagement/PatientManager.swift
@@ -13,12 +13,61 @@ struct PatientManager {
     
     // Process instructions
     mutating func process(_ instructions: [String]) -> String {
+        // Reset data
+        patients = [String: Patient]()
+        exams = [String: String]()
+        
+        instructions.forEach { instruction in
+            let instructionArr = instruction.split(separator: " ", maxSplits: 3).map{String($0)}
+            let category = instructionArr[1]
+            switch category {
+            case Category.PATIENT.rawValue:
+                processPatientData(by: instructionArr)
+            case Category.EXAM.rawValue:
+                processExamData(by: instructionArr)
+            default:
+                return
+            }
+        }
+        
         return generateSummary()
     }
     
     // Print out a summary of the patients
     private func generateSummary() -> String {
         return ""
+    }
+    
+    // Process patient related instructions
+    private mutating func processPatientData(by instruction: [String]) {
+        let operation = instruction[0]
+        let id = instruction[2]
+        switch operation {
+        case Operation.ADD.rawValue:
+            let name = instruction[3]
+            addPatient(id, name)
+        case Operation.DEL.rawValue:
+            deletePatient(id)
+        default:
+            return
+        }
+    }
+    
+    // Process exam related instructions
+    private mutating func processExamData(by instruction: [String]) {
+        let operation = instruction[0]
+        
+        switch operation {
+        case Operation.ADD.rawValue:
+            let patientId = instruction[2]
+            let examId = instruction[3]
+            addExam(patientId, examId)
+        case Operation.DEL.rawValue:
+            let examId = instruction[2]
+            deleteExam(examId)
+        default:
+            return
+        }
     }
     
     // Add a patient
