8a1b392c7014d178ceac9d9f8b5e06c85b218c14
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-04-08 14:48:25 -0700

    Add test cases for new feature

--- Test/Test.swift
+++ Test/Test.swift
@@ -6,10 +6,16 @@
 //
 
 import XCTest
+import CoreData
 @testable import PatientManagement
 final class Test: XCTestCase {
     
-    var patientManager = PatientManager()
+    var patientManager: PatientManager!
+    
+    override func setUp() {
+      super.setUp()
+        patientManager = PatientManager(viewContext: PersistenceController(inMemory: true).container.viewContext)
+    }
     
     // Test the given test case
     func testCase1() throws {
@@ -65,6 +71,33 @@ final class Test: XCTestCase {
         XCTAssertEqual(summary, expected)
     }
     
+    // Test reading multiple files
+    func testMultipleCase1() throws {
+        guard let firstInstructions = readFile(filename: "case1") else {return}
+        guard let secondInstructions = readFile(filename: "case5") else {return}
+        patientManager.process(firstInstructions)
+        let summary = patientManager.process(secondInstructions)
+        let expected = """
+        Name: JOHN DOE, Id: 123, Exam Count: 1
+        Name: JANE CROW, Id: 789, Exam Count: 2
+        """
+        XCTAssertEqual(summary, expected)
+    }
+    
+    // Test reading multiple files
+    func testMultipleCase2() throws {
+        guard let firstInstructions = readFile(filename: "case2") else {return}
+        guard let secondInstructions = readFile(filename: "case3") else {return}
+        patientManager.process(firstInstructions)
+        let summary = patientManager.process(secondInstructions)
+        let expected = """
+        Name: JOHN DOE, Id: 123, Exam Count: 0
+        Name: XIYUAN TU, Id: 234, Exam Count: 1
+        Name: JOE SCMOE, Id: 321, Exam Count: 1
+        """
+        XCTAssertEqual(summary, expected)
+    }
+    
     func readFile(filename: String) -> [String]? {
         guard let filePath = Bundle(for: type(of: self)).path(forResource: filename, ofType: "txt") else {
             XCTFail("Failed to read file")
@@ -82,6 +115,16 @@ final class Test: XCTestCase {
         }
         
         return instructions
+    }
+
+}
+
+public extension NSManagedObject {
+
+    convenience init(usedContext: NSManagedObjectContext) {
+        let name = String(describing: type(of: self))
+        let entity = NSEntityDescription.entity(forEntityName: name, in: usedContext)!
+        self.init(entity: entity, insertInto: usedContext)
     }
 
 }
