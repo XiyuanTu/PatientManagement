5507897883c50ad323713a6a362025671583b748
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-04-08 14:46:43 -0700

    Add core data stack

--- PatientManagement/Persistence.swift
+++ PatientManagement/Persistence.swift
@@ -6,17 +6,29 @@
 //
 
 import CoreData
+import Foundation
 
-struct PersistenceController {
+class PersistenceController {
     static let shared = PersistenceController()
     
+    let modelName = "PatientManager"
     let container: NSPersistentContainer
     
-    init() {
-        container = NSPersistentContainer(name: "PatientManager")
+    init(inMemory: Bool = false) {
+        let modelURL = Bundle(for: type(of: self)).url(forResource: modelName, withExtension: "momd")!
+        let model = NSManagedObjectModel(contentsOf: modelURL)!
+        
+        container = NSPersistentContainer(name: modelName, managedObjectModel: model)
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
+            } else {
+//                print(storeDescription.url!)
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
